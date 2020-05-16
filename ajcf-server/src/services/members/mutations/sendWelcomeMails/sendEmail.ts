93e875ca93f69054ecc58f86a9a8a58126dd1113
nicolas67 <nicolas@ovrsea.com>
2020-05-16 18:05:28 +0200

    feat(mail): fix mail + credentials issue, do not use aws secret anymore

--- ajcf-server/src/services/members/mutations/sendWelcomeMails/sendEmail.ts
+++ ajcf-server/src/services/members/mutations/sendWelcomeMails/sendEmail.ts
@@ -1,7 +1,10 @@
 import mailjet, { Email } from "node-mailjet";
 
 export const sendEmail = async (emailPayload: Email.SendParamsMessage) => {
-  if (process.env.ENV !== "prod") return;
+  if (process.env.ENV !== "prod") {
+    console.log("Developement environment, do not send email");
+    return;
+  }
   const mailjetClient = mailjet.connect(process.env.MAILJET_API_KEY!, process.env.MAILJET_API_SECRET!);
   return mailjetClient.post("send", { version: "v3.1" }).request({
     Messages: [emailPayload],
