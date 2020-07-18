69295ff88c094fcffbfd4ae5c059ea2fe53e41d0
nicolas67 <nicolas@ovrsea.com>
2020-07-18 18:26:40 +0200

    eng(front): split contentful dev prod

--- ajcf-site/gatsby-config.ts
+++ ajcf-site/gatsby-config.ts
@@ -49,8 +49,8 @@ export const plugins = [
     resolve: `gatsby-source-contentful`,
     options: {
       spaceId: process.env.CONTENTFUL_SPACE_ID,
-      accessToken: process.env.CONTENTFUL_DRAFT_ACCESS_TOKEN,
-      host: "preview.contentful.com",
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      host: process.env.CONTENTFUL_API_ENDPOINT,
     },
   },
   {
