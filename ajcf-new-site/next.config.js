38f1d0e756a648280bddaa36d55e01b18249b996
nicolas67 <nicolas@ovrsea.com>
2020-11-14 18:31:32 +0100

    eng: migrate to nextjs

--- ajcf-new-site/next.config.js
+++ ajcf-new-site/next.config.js
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require("path");
+const withOptimizedImages = require("next-optimized-images");
 
-module.exports = {
+module.exports = withOptimizedImages({
+  optimizeImagesInDev: false,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-};
+});
