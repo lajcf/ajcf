0cb53c555ba6eb329a2f8cfeeb058239c46a2d0d
Issei Morimoto <issei.morimoto@gmail.com>
2020-11-17 06:36:39 +0100

    feat: Styling Footer further.

--- ajcf-new-site/components/Layout/Footer/FooterComponents/Logo.tsx
+++ ajcf-new-site/components/Layout/Footer/FooterComponents/Logo.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import styles from "../Footer.module.scss";
-// import { footer__logo } from "../Footer.module.scss";
-import Image from "next/image";
 
 export const Logo = () => {
   return (
-    // <div className={styles.logo}>{/* <Image src="/img/ajcf-logo.jpg" alt="Logo" width="296" height="534" /> */}</div>
-    <div className={styles.logo}>Logo</div>
+    <figure className={styles.logo}>
+      <img src={require("../../../../public/ajcf-logo.jpg")} />
+    </figure>
   );
 };
--- ajcf-new-site/components/Layout/Footer/FooterComponents/NewsletterPlea.tsx
+++ ajcf-new-site/components/Layout/Footer/FooterComponents/NewsletterPlea.tsx
@@ -1,6 +1,22 @@
 import React from "react";
 import styles from "../Footer.module.scss";
+import { Input, Button } from "antd";
+import { RightOutlined } from "@ant-design/icons";
+
+const enterMail = <RightOutlined />;
 
 export const NewsletterPlea = () => {
-  return <div className={styles.newsletterPlea}>NewsletterPlea</div>;
+  return (
+    <div className={styles.newsletterPlea}>
+      <h3>Inscrivez-vous à notre Newsletter !</h3>
+      <div>
+        <Input.Group compact>
+          <Input placeholder="Email address" bordered={false} style={{ width: "50%" }} />
+          <Button type="primary">
+            <RightOutlined />
+          </Button>
+        </Input.Group>
+      </div>
+    </div>
+  );
 };
