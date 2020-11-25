fd918d190db5f5f8630187c28bfd9e85f9110f30
Issei Morimoto <issei.morimoto@gmail.com>
2020-11-25 05:01:18 +0100

    feat: Add link to home on header logo

--- ajcf-new-site/components/Layout/Header/Logo.tsx
+++ ajcf-new-site/components/Layout/Header/Logo.tsx
@@ -4,7 +4,7 @@ import styles from "./Header.module.scss";
 export const Logo = () => {
   return (
     <div className={styles.logo}>
-      <a>
+      <a href="/">
         <img className={styles.logoImg} src={require("../../../public/ajcf-logo.jpg")} />
       </a>
     </div>
