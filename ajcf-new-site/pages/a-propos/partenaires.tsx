dbf081c80872d6e8b0dda4ba769fdf1852001d27
Issei Morimoto <issei.morimoto@gmail.com>
2020-11-26 06:21:32 +0100

    style: Create page. Add Summary section.

---
+++ ajcf-new-site/pages/a-propos/partenaires.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Summary } from "../../components/APropos/Partners/SummaryComponents/Summary";
+import { Layout } from "../../components/Layout/Layout";
+
+export default () => {
+  return (
+    <Layout>
+      <Summary />
+    </Layout>
+  );
+};
