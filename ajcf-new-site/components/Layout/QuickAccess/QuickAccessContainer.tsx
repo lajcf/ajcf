fd5a0e44defc400e698474c7b19cb8e7c6fe241c
Issei Morimoto <issei.morimoto@gmail.com>
2020-12-04 07:25:03 +0100

    fix: Parse Pathname from the start

--- ajcf-new-site/components/Layout/QuickAccess/QuickAccessContainer.tsx
+++ ajcf-new-site/components/Layout/QuickAccess/QuickAccessContainer.tsx
@@ -9,8 +9,10 @@ export type SubPage = "qui-sommes-nous" | "partenaires" | "presse" | undefined;
 const parsePathname = () => {
   const { pathname } = useRouter();
   const dividedPathname = pathname.split("/");
-  const page = dividedPathname.slice(-2, -1)[0] as Page;
-  const subPage = dividedPathname.slice(-1)[0] as SubPage;
+  const page = dividedPathname[1] as Page;
+  const subPage = dividedPathname[2] as SubPage;
+  console.log(page);
+  console.log(subPage);
   return {
     page: page,
     subPage: subPage,
