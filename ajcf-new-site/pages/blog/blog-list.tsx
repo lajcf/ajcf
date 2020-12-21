d0ea74ffb9ad3364945d9a0b80b0e259ef98825b
Issei <issei.morimoto@gmail.com>
2020-12-21 15:07:16 +0100

    eng: WIP Implementing blog-list

--- ajcf-new-site/pages/blog/blog-list.tsx
+++ ajcf-new-site/pages/blog/blog-list.tsx
@@ -1,25 +1,39 @@
 import { GetStaticProps } from "next";
 import React from "react";
+import { truncate } from "lodash";
 import { graphqlClient } from "../../lib/graphql/graphqlClient";
 import { Article } from "../../types/types";
+import styles from "../../components/Blog/BlogListComponents/BlogList.module.scss";
+
+const filterContentSummary = (content: string) => {
+  const truncatedContentSummary = truncate(content, { length: 140 });
+  return truncatedContentSummary.replace("\\n", ". ");
+};
 
 export const ArticlePreview = ({ article }: { article: Article }) => {
-  console.log(article);
+  const contentSummary = filterContentSummary(article.content.text);
   return (
-    <div>
-      <h2>{article.title}</h2>
-      <p>{article.author}</p>
-      <img src={article.cover.url} />
+    <div className={styles.preview}>
+      {article.cover?.url && (
+        <div className={styles.previewCover}>
+          <img src={article.cover.url} />
+        </div>
+      )}
+      <h2 className={styles.previewTitle}>{article.title}</h2>
+      <p className={styles.previewAuthor}>{article.author}</p>
+      <small className={styles.previewContentSummary}>{contentSummary}</small>
     </div>
   );
 };
 
 export default function BlogList({ articles }: { articles: Article[] }) {
   return (
-    <>
+    <main className={styles.blogList}>
       <h1>Blog</h1>
-      <ArticlePreview article={articles[0]} />
-    </>
+      <section className={styles.articlesPreviews}>
+        <ArticlePreview article={articles[0]} />
+      </section>
+    </main>
   );
 }
 
