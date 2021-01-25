cf3b2ff9a0b0635cd4ea97b8c069ce943cae2248
Issei <issei.morimoto@gmail.com>
2021-01-25 04:52:08 +0100

    style: Style for quickAccess in OurEspaces

--- ajcf-new-site/components/ProjetsAJCF/OurEspacesComponents/OurEspacesContainer.tsx
+++ ajcf-new-site/components/ProjetsAJCF/OurEspacesComponents/OurEspacesContainer.tsx
@@ -2,23 +2,22 @@ import React from "react";
 import { Espace } from "./Espace";
 import { ourEspacesContent } from "./ourEspacesConfig";
 import { Layout } from "../../Layout/Layout";
+import styles from "./OurEspaces.module.scss";
 
 export const OurEspacesContainer = () => {
   return (
-    <Layout>
-      <main>
-        <section>
-          <h1>Nos espaces</h1>
-          <p className="texte1">
-            Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum
-            ponderum. Est audiam animal molestiae te. Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad,
-            nam no suscipit quaerendum. At nam minimum ponderum
-          </p>
-        </section>
-        {ourEspacesContent.map((espace) => (
-          <Espace key={espace.categoryName} espaceContent={espace} />
-        ))}
-      </main>
+    <Layout className={styles.layout}>
+      <section className={styles.summarySection}>
+        <h1>Nos espaces</h1>
+        <p className="texte1">
+          Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum
+          ponderum. Est audiam animal molestiae te. Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam
+          no suscipit quaerendum. At nam minimum ponderum
+        </p>
+      </section>
+      {ourEspacesContent.map((espace) => (
+        <Espace key={espace.categoryName} espaceContent={espace} />
+      ))}
     </Layout>
   );
 };
