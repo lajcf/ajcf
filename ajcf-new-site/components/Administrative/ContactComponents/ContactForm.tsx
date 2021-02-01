14425f74012a0a0fcfcacae086cb663f9b53d4f6
Issei <issei.morimoto@gmail.com>
2021-02-02 00:42:32 +0100

    eng: Add rules to contactForm

--- ajcf-new-site/components/Administrative/ContactComponents/ContactForm.tsx
+++ ajcf-new-site/components/Administrative/ContactComponents/ContactForm.tsx
@@ -14,19 +14,35 @@ export const ContactForm = ({ handleSubmit, isLoading }: ContactFormProps) => {
       <Form.Item label="Don't fill this out" className="hidden" style={{ display: `none` }} name="botField">
         <Input type="hidden" />
       </Form.Item>
-      <Form.Item name="lastName" className={styles.lastName}>
+      <Form.Item
+        name="lastName"
+        className={styles.lastName}
+        rules={[{ required: true, message: "Veuillez indiquer votre nom de famille" }]}
+      >
         <Input placeholder="Nom*" />
       </Form.Item>
-      <Form.Item name="firstName" className={styles.firstName}>
+      <Form.Item
+        name="firstName"
+        className={styles.firstName}
+        rules={[{ required: true, message: "Veuillez indiquer votre prénom" }]}
+      >
         <Input placeholder="Prénom*" />
       </Form.Item>
-      <Form.Item name="mailAddress" className={styles.mailAddress}>
+      <Form.Item
+        name="mailAddress"
+        className={styles.mailAddress}
+        rules={[{ type: "email", message: "Veuillez indiquer votre adresse e-mail" }]}
+      >
         <Input placeholder="Adresse e-mail*" />
       </Form.Item>
       <Form.Item name="messageObject" className={styles.messageObject}>
         <Input placeholder="Objet du message*" />
       </Form.Item>
-      <Form.Item name="messageContent" className={styles.messageContent}>
+      <Form.Item
+        name="messageContent"
+        className={styles.messageContent}
+        rules={[{ required: true, message: "Veuillez écrire le contenu de votre message" }]}
+      >
         <Input.TextArea placeholder="Message*" />
       </Form.Item>
       <div className={styles.formBottom}>
