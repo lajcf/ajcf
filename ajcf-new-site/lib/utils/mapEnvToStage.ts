2aa0cfb23a7508b6dbc7fa7edd7fdce35a4f7453
Nicolas Li <nicolas@ovrsea.com>
2021-02-13 21:00:15 +0100

    eng: implement stage handling

--- ajcf-new-site/lib/utils/mapEnvToStage.ts
+++ ajcf-new-site/lib/utils/mapEnvToStage.ts
@@ -1,3 +1,6 @@
 import { Stage } from "../../types/types";
 
-export const mapEnvToStage = (env?: string): Stage => (env === "prod" ? Stage.Published : Stage.Draft);
+export const mapEnvToStage = (env?: string): Stage => {
+  if (!env) throw new Error("ENV variable should be set.");
+  return env === "prod" ? Stage.Published : Stage.Draft;
+};
