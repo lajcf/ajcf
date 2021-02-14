a02276365846c341ba3df8227381b3f0a19a16a2
Nicolas Li <nicolas@ovrsea.com>
2021-02-14 18:53:11 +0100

    eng(devops): make cache available in prod

--- ajcf-server/src/services/helloAsso/fetchActions.ts
+++ ajcf-server/src/services/helloAsso/fetchActions.ts
@@ -15,6 +15,7 @@ export const fetchActions = async (args: GetActionsInterface): Promise<HelloAsso
   const encodedAuth = Buffer.from(`${process.env.HELLOASSO_USERNAME}:${process.env.HELLOASSO_PASSWORD}`).toString(
     "base64"
   );
+
   const url = GET_ACTIONS_URL(args.campaignId, args.actionType);
   try {
     const result = await axios.get<HelloAssoActionsQueryResponse>(url, {
