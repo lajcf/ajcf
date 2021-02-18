038a06401c2b3575e242591e304f43f439297e11
Issei <issei.morimoto@gmail.com>
2021-02-18 12:50:44 +0100

    style: Implement carousel arrows

--- ajcf-new-site/components/Home/HomeComponents/CarouselComponents/CarouselContainer.tsx
+++ ajcf-new-site/components/Home/HomeComponents/CarouselComponents/CarouselContainer.tsx
@@ -1,17 +1,26 @@
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 import { Carousel } from "antd";
-import React from "react";
+import React, { useRef } from "react";
 import { ArticlePreviewFragment } from "../../../../types/types";
 import styles from "./Carousel.module.scss";
 import { CarouselItem } from "./CarouselItem";
 
 export const CarouselContainer = ({ articles }: { articles: ArticlePreviewFragment[] }) => {
+  const slider = useRef() as any;
   return (
-    <section className={styles.carouselContainer}>
-      <Carousel className={styles.carousel}>
+    <section className={styles.carouselSection}>
+      <LeftOutlined className={styles.leftArrow} onClick={() => slider.current.prev()} />
+      <Carousel
+        className={styles.carousel}
+        ref={(ref) => {
+          slider.current = ref;
+        }}
+      >
         {articles.map((article) => (
           <CarouselItem article={article} key={article.id} />
         ))}
       </Carousel>
+      <RightOutlined className={styles.rightArrow} onClick={() => slider.current.next()} />
     </section>
   );
 };
