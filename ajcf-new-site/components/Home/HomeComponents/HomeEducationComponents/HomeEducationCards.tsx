6841f2db555b5e1544e7106a994fa2547a9f40d1
Issei Morimoto <issei.morimoto@gmail.com>
2021-03-04 18:02:04 +0100

    style: HomeEducation

--- ajcf-new-site/components/Home/HomeComponents/HomeEducationComponents/HomeEducationCards.tsx
+++ ajcf-new-site/components/Home/HomeComponents/HomeEducationComponents/HomeEducationCards.tsx
@@ -5,30 +5,61 @@ const homeEducationCards = [
   {
     id: 1,
     title: "Interventions scolaires",
-    subTitle: "En partenariat avec le Ministère de l'Éducation",
-    content: "yo",
+    subTitle: (
+      <h4>
+        En partenariat avec{" "}
+        <a href="https://www.education.gouv.fr/" target="_blank" rel="noreferrer noopener">
+          le Ministère de l'Éducation
+        </a>
+      </h4>
+    ),
+    link: "https://www.education.gouv.fr/",
+    content:
+      "Le pôle Education réalise des interventions en milieu scolaire pour sensibiliser la jeunesse à l’histoire de l’immigration des Asiatiques, favoriser le dialogue multiculturel et intergénérationnel, et déconstruire les stéréotypes, afin de promouvoir le bien vivre-ensemble.",
+    image: require("../../../../public/Images/Accueil/accueil_interventions-scolaires.jpg"),
   },
   {
     id: 2,
     title: "Parcours de Vie",
-    subTitle: "En partenariat avec SOS Racisme",
-    content: "ya",
+    subTitle: (
+      <h4>
+        En partenariat avec{" "}
+        <a href="https://sos-racisme.org/" target="_blank" rel="noreferrer noopener">
+          SOS Racisme
+        </a>
+      </h4>
+    ),
+    link: "https://sos-racisme.org/",
+    content: "Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum.",
+    image: require("../../../../public/Images/Accueil/accueil_parcours-de-vie.jpg"),
   },
 ];
+
+const oddOrEvenCard = (id: number) => {
+  return id % 2 ? styles.cardOdd : styles.cardEven;
+};
+
 export const HomeEducationCards = () => {
   return (
-    <section className={styles.cardsSection}>
+    <div className={styles.cardsSection}>
       <ul>
         {homeEducationCards.map((card) => (
-          <li key={card.id}>
+          <li key={card.id} className={`${styles.card} ${oddOrEvenCard(card.id)}`}>
+            <div className={styles.cardImage}>
+              <img src={card.image} />
+            </div>
+            <div className={styles.emptyZone} />
             <div className={styles.cardText}>
               <h3>{card.title}</h3>
-              <h4>{card.subTitle}</h4>
+              {card.subTitle}
               <p>{card.content}</p>
+              <button className={styles.cardButton} type="button">
+                En savoir plus
+              </button>
             </div>
           </li>
         ))}
       </ul>
-    </section>
+    </div>
   );
 };
--- ajcf-new-site/components/Home/HomeComponents/HomeEducationComponents/HomeEducationContainer.tsx
+++ ajcf-new-site/components/Home/HomeComponents/HomeEducationComponents/HomeEducationContainer.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import styles from "./HomeEducation.module.scss";
 import { HomeEducationCards } from "./HomeEducationCards";
 
-const textContent = `Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum. Est audiam animal molestiae te. Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum. Est audiam animal molestiae te.`;
+const textContent = `La Covid-19 a exacerbé une réalité qui existait déjà : le racisme anti-asiatique. Les actions menées par le Pôle Éducation ont pour but de prévenir ce phénomène, par le biais d’interventions en milieu scolaire autour de thèmes comme l’histoire de l’immigration des Asiatiques, la déconstruction des stéréotypes, et le bien-vivre ensemble. 
+`;
 export const HomeEducationContainer = () => {
   return (
     <section className={styles.educationSection}>
-      <section className={styles.presentationSection}>
+      <div className={styles.presentationSection}>
         <h2 className="capsHeading">Education AJCF</h2>
         <h1>Sensibiliser aux discriminations et racisme anti-asiatique</h1>
         <p>{textContent}</p>
-      </section>
+      </div>
       <HomeEducationCards />
     </section>
   );
