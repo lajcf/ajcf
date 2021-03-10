78ecfe8ec9c6b640cc20e0b17def2cfedbeb4fe2
Issei Morimoto <issei.morimoto@gmail.com>
2021-03-10 18:38:28 +0100

    style: ContactUs section

--- ajcf-new-site/components/APropos/OurStory/ContactUsComponents/OurStoryContactUsContainer.tsx
+++ ajcf-new-site/components/APropos/OurStory/ContactUsComponents/OurStoryContactUsContainer.tsx
@@ -1,13 +1,16 @@
+import Link from "next/link";
 import React from "react";
 import styles from "../OurStory.module.scss";
 
-const pleaContent = `Intéréssé(e) pour nous rejoindre ? Vous aussi,\nvous voulez contribuer à nos projets ?`;
+const pleaContent = `Intéréssé(e) pour nous rejoindre ? Vous aussi, vous voulez contribuer à nos projets ?`;
 
 export const OurStoryContactUsContainer = () => {
   return (
     <section className={styles.contactUsSection}>
-      <h3>{pleaContent}</h3>
-      <button type="button">Contactez-nous !</button>
+      <h2>{pleaContent}</h2>
+      <Link href="/contact">
+        <button type="button">Contactez-nous !</button>
+      </Link>
     </section>
   );
 };
