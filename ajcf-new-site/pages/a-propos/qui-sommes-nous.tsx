852eb95083401ef3b63745f7d9419a0ea2063351
Issei Morimoto <issei.morimoto@gmail.com>
2021-03-23 19:03:43 +0100

    style: WhoAreWe

--- ajcf-new-site/pages/a-propos/qui-sommes-nous.tsx
+++ ajcf-new-site/pages/a-propos/qui-sommes-nous.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { WhoAreWeContainer } from "../../components/APropos/WhoAreWe/ProjetsAJCFComponents/ProjetsAJCFContainer";
+import { WhoAreWeContainer } from "../../components/APropos/WhoAreWe/WhoAreWeComponents/WhoAreWeContainer";
 
 export default function whoAreWe() {
   return <WhoAreWeContainer />;
