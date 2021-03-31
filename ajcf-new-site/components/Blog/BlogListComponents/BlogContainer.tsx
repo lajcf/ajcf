d67fa42efc752c44d9b3bf1db93f57294b24cb2f
Issei Morimoto <issei.morimoto@gmail.com>
2021-03-31 20:18:43 +0200

    enh: externalize blog functions WIP

--- ajcf-new-site/components/Blog/BlogListComponents/BlogContainer.tsx
+++ ajcf-new-site/components/Blog/BlogListComponents/BlogContainer.tsx
@@ -1,11 +1,22 @@
 import { uniq, sortBy } from "lodash";
 import React, { useState } from "react";
 import { NUMBER_OF_ARTICLES_TO_DISPLAY_AT_A_TIME } from "../../../lib/constants";
-import { ArticlePreviewFragment, BlogCategory, BlogLabel } from "../../../types/types";
+import {
+  ArticlePreviewFragment,
+  BlogCategory,
+  BlogLabel,
+  EventCategory,
+  EventLabel,
+  EventPreviewFragment,
+} from "../../../types/types";
 import { Layout } from "../../Layout/Layout";
 import { ArticlePreview } from "./ArticlePreview";
 import styles from "./BlogContainer.module.scss";
-import { CategoryLabelSelector } from "./SelectorBlogCategoryOrLabel";
+import { SelectorForCategoryOrLabel } from "../../../lib/utils/ItemsPreviewsListComponents/SelectorBlogCategoryOrLabel";
+
+type Item = ArticlePreviewFragment | EventPreviewFragment;
+export type Category = BlogCategory | EventCategory;
+export type Label = BlogLabel | EventLabel;
 
 const limitDisplayedArticles = (filteredArticles: ArticlePreviewFragment[], numberOfArticlesToDisplay: number) => {
   return filteredArticles.slice(0, numberOfArticlesToDisplay);
@@ -32,11 +43,35 @@ const filterArticles = (
 const selectUsedLabels = (articles: ArticlePreviewFragment[]) =>
   uniq(articles.flatMap((article) => article.blogLabels));
 
+const filterItems = (items: Item[], selectedCategory?: Category, selectedLabel?: Label) => {
+  if (selectedCategory) {
+    return items.filter((item) =>
+      "blogCategory" in item
+        ? item.blogCategory.includes(selectedCategory)
+        : item.eventCategory.includes(selectedCategory)
+    );
+  }
+  if (selectedLabel) {
+    return items.filter((item) =>
+      "blogLabels" in item ? item.blogLabels.includes(selectedLabel) : item.eventLabels.includes(selectedLabel)
+    );
+  }
+  return items;
+};
+const processItems = (items: Item[], selectedCategory, selectedLabel) => {
+  const orderedItems = sortBy(items, ["optionalPublishedDate", "createdAt"]);
+  const filteredItems = filterItems(orderedItems, selectedCategory, selectedLabel);
+  console.log(filteredItems);
+  return items;
+};
+
 export const BlogContainer = ({ articles }: { articles: ArticlePreviewFragment[] }) => {
-  const [selectedCategory, setSelectedCategory] = useState<BlogCategory>();
-  const [selectedLabel, setSelectedLabel] = useState<BlogLabel>();
+  const [selectedCategory, setSelectedCategory] = useState<Category>();
+  const [selectedLabel, setSelectedLabel] = useState<Label>();
   const [numberOfArticlesToDisplay, setNumberOfArticlesToDisplay] = useState(NUMBER_OF_ARTICLES_TO_DISPLAY_AT_A_TIME);
 
+  const itemsToShow = processItems(articles, selectedCategory, selectedLabel);
+  console.log(itemsToShow);
   const orderedArticles = orderArticles(articles);
   const filteredArticles = filterArticles(orderedArticles, selectedCategory, selectedLabel);
   const displayedArticles = limitDisplayedArticles(filteredArticles, numberOfArticlesToDisplay);
@@ -46,23 +81,24 @@ export const BlogContainer = ({ articles }: { articles: ArticlePreviewFragment[]
     setSelectedLabel(undefined);
   };
 
-  const handleSelectCategory = (category: BlogCategory) => {
+  const handleSelectedCategory = (category: Category) => {
     resetFilters();
     setSelectedCategory(category);
   };
 
-  const handleSelectLabel = (label: BlogLabel) => {
+  const handleSelectedLabel = (label: Label) => {
     resetFilters();
     setSelectedLabel(label);
   };
 
   return (
     <Layout>
-      <CategoryLabelSelector
+      <SelectorForCategoryOrLabel
         selectedLabel={selectedLabel}
         selectedCategory={selectedCategory}
-        handleSelectLabel={handleSelectLabel}
-        handleSelectCategory={handleSelectCategory}
+        handleSelectedLabel={handleSelectedLabel}
+        handleSelectedCategory={handleSelectedCategory}
+        categoriesToShow={Object.values(BlogCategory)}
         labelsToShow={selectUsedLabels(articles)}
         resetFilters={resetFilters}
       />
