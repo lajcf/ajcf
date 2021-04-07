158c2e860057cd719d4b52037fa4af92ac2f33c7
Issei Morimoto <issei.morimoto@gmail.com>
2021-04-07 16:16:37 +0200

    fix: number of recent press files in constant

--- ajcf-new-site/components/APropos/Press/PressContainer.tsx
+++ ajcf-new-site/components/APropos/Press/PressContainer.tsx
@@ -5,6 +5,8 @@ import { Layout } from "../../Layout/Layout";
 import styles from "./Press.module.scss";
 import { PressFiles } from "./PressFiles";
 
+const NUMBER_OF_RECENT_PRESS_FILES = 3;
+
 const selectPressFiles = (pressFiles: PressFileFragment[], label: AssetLabel): PressFileFragment[] => {
   return pressFiles.filter((pressFile) => pressFile.assetLabel.includes(label));
 };
@@ -32,7 +34,7 @@ export const PressContainer = ({ pressFiles }: { pressFiles: PressFileFragment[]
           </li>
         </ul>
       </section>
-      <PressFiles pressFiles={orderedPressFiles.slice(0, 3)} title="Les plus récents" />
+      <PressFiles pressFiles={orderedPressFiles.slice(0, NUMBER_OF_RECENT_PRESS_FILES)} title="Les plus récents" />
       <PressFiles pressFiles={selectPressFiles(orderedPressFiles, AssetLabel.PressReview)} title="Revue de presse" />
       <PressFiles pressFiles={selectPressFiles(orderedPressFiles, AssetLabel.PressRelease)} title="Communiqués" />
     </Layout>
