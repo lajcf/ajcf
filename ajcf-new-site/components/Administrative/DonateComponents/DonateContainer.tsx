48426c20d0a641783138e38f1ed3be642d2d45c8
Issei Morimoto <issei.morimoto@gmail.com>
2021-05-03 18:45:02 +0200

    style-🎨: stylize donate

--- ajcf-new-site/components/Administrative/DonateComponents/DonateContainer.tsx
+++ ajcf-new-site/components/Administrative/DonateComponents/DonateContainer.tsx
@@ -9,10 +9,18 @@ export const DonateContainer = () => {
     <Layout className={styles.layout}>
       <section className={styles.summarySection}>
         <h1>Faire un don pour nous soutenir</h1>
-        <p className="texte1">
-          Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum
-          ponderum. Est audiam animal molestiae te. Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam
-          no suscipit quaerendum. At nam minimum ponderum
+        <p>
+          Si vous souhaitez nous aider, sans avoir le temps nécessaire pour devenir un membre actif, vous pouvez faire
+          un don pour aider l’AJCF à grandir, se développer, et améliorer l’efficacité de ses actions.
+        </p>
+        <p>
+          Soutenir l’AJCF, c’est aider à :
+          <ul>
+            <li>une meilleure représentation de la communauté chinoise et asiatique</li>
+            <li>lutter contre le racisme et les discriminations</li>
+            <li>consolider un réseau solidaire d’aide aux victimes</li>
+            <li>contribuer à des missions éducatives envers tous les publics.</li>
+          </ul>
         </p>
       </section>
       <DonationIncentives />
