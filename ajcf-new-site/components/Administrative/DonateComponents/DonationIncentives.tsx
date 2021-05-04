018974a21ced7474c489a5486ce2ee0aa914db9f
Issei Morimoto <issei.morimoto@gmail.com>
2021-05-04 21:18:34 +0200

    style-🎨: stylize donate (#125)

--- ajcf-new-site/components/Administrative/DonateComponents/DonationIncentives.tsx
+++ ajcf-new-site/components/Administrative/DonateComponents/DonationIncentives.tsx
@@ -1,25 +1,33 @@
 import React from "react";
 import styles from "./Donate.module.scss";
 
+const incentivesList = [
+  {
+    bigFigure: "66%",
+    text:
+      "de réduction d'impôt sur le total des versements, dans la limite de 20 % du revenu imposable de votre foyer.",
+  },
+  {
+    bigFigure: "17 €",
+    text: "de coût réel pour un don de 50€.",
+  },
+  {
+    bigFigure: "1 reçu fiscal",
+    text: "que l’on vous envoie systématiquement par mail après chaque don.",
+  },
+];
+
 export const DonationIncentives = () => {
   return (
     <section className={styles.donationIncentivesSection}>
       <h1>Des versements déduits de vos impôts</h1>
       <ul>
-        <li>
-          <div className={styles.bigFigure}>66%</div>
-          <p className="texte1">
-            de réduction d'impôt sur le total des versements, dans la limite de 20 % du revenu imposable de votre foyer.{" "}
-          </p>
-        </li>
-        <li>
-          <div className={styles.bigFigure}>17 €</div>
-          <p className="texte1">de coût réel pour un don de 50€.</p>
-        </li>
-        <li>
-          <div className={styles.bigFigure}>1 reçu fiscal</div>
-          <p className="texte1">que l’on vous envoie systématiquement par mail après chaque don.</p>
-        </li>
+        {incentivesList.map((incentive) => (
+          <li key={incentive.text}>
+            <div className={styles.bigFigure}>{incentive.bigFigure}</div>
+            <p>{incentive.text}</p>
+          </li>
+        ))}
       </ul>
     </section>
   );
