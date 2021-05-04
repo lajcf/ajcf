29fb15c919f465e02870b2889e5b3909bf1b4e44
lajcf <64323525+lajcf@users.noreply.github.com>
2021-05-04 21:18:05 +0200

    chore-🔧(site): allow crawlers only for prod (#123)
    Co-authored-by: Nicolas Li <nicolas@ovrsea.com>

--- ajcf-new-site/pages/_app.tsx
+++ ajcf-new-site/pages/_app.tsx
@@ -1,9 +1,19 @@
 import "../styles/index.scss";
 import { AppProps } from "next/app";
 import React from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {process.env.NEXT_PUBLIC_ENV !== "prod" && (
+        <Head>
+          <meta name="robots" content="noindex" />
+        </Head>
+      )}
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
