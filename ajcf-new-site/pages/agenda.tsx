0f69ccf8da02ff6c929cdbf775f8745439e95867
Nicolas Li <nicolas@ovrsea.com>
2021-05-09 17:42:18 +0200

    fix-🐛(site): test remove revalidate

--- ajcf-new-site/pages/agenda.tsx
+++ ajcf-new-site/pages/agenda.tsx
@@ -2,18 +2,24 @@ import { GetStaticProps } from "next";
 import React from "react";
 import { AgendaContainer } from "../components/Agenda/AgendaComponents/AgendaContainer";
 import { graphqlClient } from "../lib/graphql/graphqlClient";
-import { EventPreviewFragment } from "../types/types";
+import { EventPreviewFragment, EventsPreviewQueryDocument, EventsPreviewQueryQuery } from "../types/types";
+import { useActualItem } from "../lib/graphql/useActualItem";
 
-type AgendaProps = { events: EventPreviewFragment[] };
-export default function Agenda({ events }: AgendaProps) {
-  return <AgendaContainer events={events} />;
+type AgendaProps = { initialEvents: EventPreviewFragment[] };
+
+export default function Agenda({ initialEvents }: AgendaProps) {
+  const { actualItem: actualEvents } = useActualItem<EventsPreviewQueryQuery>(EventsPreviewQueryDocument, {
+    events: initialEvents,
+  });
+  if (!actualEvents?.events) throw new Error(`List of events not found`);
+  return <AgendaContainer events={actualEvents.events} />;
 }
 
 export const getStaticProps: GetStaticProps<AgendaProps> = async () => {
   const eventsResult = await graphqlClient.eventsPreviewQuery();
   return {
     props: {
-      events: eventsResult.events,
+      initialEvents: eventsResult.events,
     },
   };
 };
