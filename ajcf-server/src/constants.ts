52a6a5fbfab56812dfa99d70a71105bd5de9cd92
Nicolas Li <nicolas@ovrsea.com>
2021-08-14 17:46:59 +0200

    fix(member): query as many members as there are

--- ajcf-server/src/constants.ts
+++ ajcf-server/src/constants.ts
@@ -1,4 +1,4 @@
 export const MAILJET_NEWSLETTER_ID = "10619";
-export const MAILJET_MEMBERS_ID = "6801";
+export const MAILJET_MEMBERS_ID = "40549";
 
 export type ActivityHeader = "subscription_reminder_sent";
