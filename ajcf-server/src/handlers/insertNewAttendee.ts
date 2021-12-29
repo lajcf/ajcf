900fac46a37e6475eb22f058308ee0d6e8cb4dc7
Nicolas Li <nicolas.li@hotmail.fr>
2021-12-29 17:10:04 +0100

    chore: refactor events handlers

---
+++ ajcf-server/src/handlers/insertNewAttendee.ts
@@ -0,0 +1,46 @@
+import { EventBridgeEvent } from "aws-lambda";
+import { uniqBy } from "lodash";
+import { getRepository } from "typeorm";
+import { HelloAssoOrder } from "../types";
+import { closeConnectionToDb, openConnectionToDb } from "../utils/dbHandlers";
+import { upsertAttendees } from "../services/attendees/mutations/upsertAttendees";
+import { formatOrderToAttendees } from "../services/helloAsso/v5/mappers/mapHelloAssoItemToAttendee";
+import { addContactsToMailingList } from "../services/mailClient/sendInBlue/addContactsToMailingList/addContactsToMailingList";
+import { Event } from "../entities/Event";
+import { upsertTickets } from "../services/tickets/upsertTickets";
+import { formatOrderToTickets } from "../services/helloAsso/v5/mappers/mapHelloAssoItemToTicket";
+import { updateEvent } from "../services/events/mutations/updateEvent";
+
+const shouldProcessEvent = (eventType: "newMember" | "newAttendee" | string) => {
+  if (eventType === "newAttendee") {
+    return true;
+  }
+  return false;
+};
+
+export const handler = async (event: EventBridgeEvent<"newMember" | "newAttendee" | string, HelloAssoOrder>) => {
+  console.log("Event received: ", JSON.stringify(event));
+  if (!shouldProcessEvent(event["detail-type"])) {
+    console.log(`Cannot process eventType: ${event["detail-type"]}`);
+    return;
+  }
+  try {
+    await openConnectionToDb();
+    const upsertedAttendees = await upsertAttendees(formatOrderToAttendees(event.detail));
+    const ajcfEvent = await getRepository(Event).findOneOrFail({ slug: event.detail.formSlug });
+    const upsertedTickets = await upsertTickets(formatOrderToTickets(event.detail, ajcfEvent, upsertedAttendees));
+    await updateEvent({
+      ...ajcfEvent,
+      tickets: upsertedTickets,
+    });
+    await addContactsToMailingList({
+      contactsToAdd: uniqBy(upsertedAttendees, (attendee) => attendee.email),
+      listId: ajcfEvent.mailjetListId,
+    });
+    await closeConnectionToDb();
+  } catch (e) {
+    console.log(e);
+    await closeConnectionToDb();
+    throw e;
+  }
+};
--- ajcf-server/src/services/helloAsso/v5/mappers/mapHelloAssoItemToAttendee.ts
+++ ajcf-server/src/services/helloAsso/v5/mappers/mapHelloAssoItemToAttendee.ts
@@ -1,6 +1,8 @@
+import { uniqBy } from "lodash";
 import dayjs from "../../../../utils/dayjs";
 import { HelloAssoSoldItem } from "../resources";
 import { Attendee } from "../../../../entities/Attendee";
+import { HelloAssoOrder } from "../../../../types";
 
 export const formatTicketToAttendee = (action: HelloAssoSoldItem): Partial<Attendee> => ({
   email: action.payer.email.toLowerCase(),
@@ -8,3 +10,14 @@ export const formatTicketToAttendee = (action: HelloAssoSoldItem): Partial<Atten
   lastName: action.user.lastName,
   lastParticipationDate: dayjs.utc(action.order.date).toDate(),
 });
+
+export const formatOrderToAttendees = (order: HelloAssoOrder): Partial<Attendee>[] =>
+  uniqBy(
+    order.items.map((item) => ({
+      email: order.payer.email.toLowerCase(),
+      firstName: item.user.firstName,
+      lastName: item.user.lastName,
+      lastParticipationDate: dayjs.utc(order.date).toDate(),
+    })),
+    (attendee) => `${attendee.email} ${attendee.firstName}`
+  );
